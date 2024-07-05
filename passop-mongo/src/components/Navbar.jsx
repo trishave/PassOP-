0b762de28eafc65f9f5dc427af2c9f6d6e984a4b
Trisha Verma <128292311+trishave@users.noreply.github.com>
2024-07-06 00:44:38 +0530

    Add files via upload

---
+++ passop-mongo/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='bg-slate-800 text-white'>
+            <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+                <div className='logo font-bold text-white text-2xl '>
+                    <span className='text-green-500'>&lt;</span>
+                    Pass
+                    <span className='text-green-500'>OP/&gt;</span>
+                </div>
+                {/* <ul>
+                    <li className='flex gap-3'>
+                        <a className="hover: font-bold" href="/">Home</a >
+                        <a className="hover: font-bold" href="#">About</a >
+                        <a className="hover: font-bold" href="#">Contact</a >
+                    </li>
+                </ul> */}
+                <button className='text-white bg-green-800 my-5 rounded-full flex justify-between items-center'>
+                    <img className='invert p-1 w-10' src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg" alt="github logo" />
+                    <span className='font-bold px-2 '>GitHub</span>
+                </button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
